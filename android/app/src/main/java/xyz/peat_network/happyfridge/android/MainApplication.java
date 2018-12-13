b6d1c4625660c2c95e81c9a096d18e16aa71c84e
Ratchanan Srirattanamet <peathot@hotmail.com>
2018-12-14 01:28:06 +0700

    Link react-native-gesture-handler

--- android/app/src/main/java/xyz/peat_network/happyfridge/android/MainApplication.java
+++ android/app/src/main/java/xyz/peat_network/happyfridge/android/MainApplication.java
@@ -3,6 +3,7 @@ package xyz.peat_network.happyfridge.android;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import io.invertase.firebase.RNFirebasePackage;
 
 import com.facebook.react.ReactNativeHost;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGestureHandlerPackage(),
           new RNFirebasePackage(),
 
           // RNFirebase packages. Keep this list sorted.
