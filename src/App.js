13fc727147b05d2d0cda1fb365ee79d827db553a
Ratchanan Srirattanamet <peathot@hotmail.com>
2018-12-14 13:57:21 +0700

    Implement auth redirection logic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { createAppContainer } from 'react-navigation';
+import { createAppContainer, NavigationActions } from 'react-navigation';
+import firebase from 'react-native-firebase';
+
+const auth = firebase.auth();
 
 import { RootNavigator } from './navigators';
 
@@ -7,6 +10,47 @@ const NavigationContainer = createAppContainer(RootNavigator);
 
 class App extends React.Component {
   containerRef = React.createRef();
+  previousAuth = undefined;
+  authSubscription = null;
+
+  navigateTo(routeName) {
+    const container = this.containerRef.current;
+    if (!container) {
+      throw new Error('NavigationContainer missing?');
+    }
+
+    container.dispatch(
+      NavigationActions.navigate({
+        routeName,
+      })
+    );
+  }
+
+  handleAuthStateChanged = (auth) => {
+    if (this.previousAuth === auth) {
+      return;
+    }
+
+    if (auth) {
+      this.navigateTo('MainNavigator');
+    }
+    else {
+      this.navigateTo('LoginScreen');
+    }
+
+    this.previousAuth = auth;
+  }
+
+  componentDidMount() {
+    this.authSubscription =
+      auth.onAuthStateChanged(this.handleAuthStateChanged);
+  }
+
+  componentWillUnmount() {
+    if (this.authSubscription) {
+      this.authSubscription();
+    }
+  }
 
   render() {
     return <NavigationContainer ref={this.containerRef} />;
