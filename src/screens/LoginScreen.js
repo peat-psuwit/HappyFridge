6e3271dd34ea1d53795ed811bc6c2027ed72f0ae
Ratchanan Srirattanamet <peathot@hotmail.com>
2018-12-14 14:25:28 +0700

    LoginScreen: implement temporary anonymous signIn

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,13 +1,36 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import firebase from 'react-native-firebase';
+
+const auth = firebase.auth();
+
+function loginAnonymously() {
+  auth.signInAnonymously()
+    .catch((e) => {
+      console.log("Can't signin (why?): ", e);
+    });
+}
 
 function LoginScreen() {
   // Stub
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.container}>
+      <Text>Login anonymously (temporary)</Text>
+      <Button title="Login" onPress={loginAnonymously} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
 
 export default LoginScreen;
