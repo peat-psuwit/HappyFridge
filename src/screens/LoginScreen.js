6a6b3e469ca3931ae7ee582eab7ad99efe8c4f02
Ratchanan Srirattanamet <peathot@hotmail.com>
2018-12-14 22:20:23 +0700

    LoginScreen: implement UI

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -5,31 +5,56 @@ import {
   Text,
   View,
 } from 'react-native';
+import {
+  GoogleSigninButton,
+} from 'react-native-google-signin';
 import firebase from 'react-native-firebase';
 
 const auth = firebase.auth();
 
-function loginAnonymously() {
-  auth.signInAnonymously()
-    .catch((e) => {
-      console.log("Can't signin (why?): ", e);
-    });
-}
+class LoginScreen extends React.Component {
+  state = {
+    isSigninInProgress: false,
+  };
 
-function LoginScreen() {
-  // Stub
-  return (
-    <View style={styles.container}>
-      <Text>Login anonymously (temporary)</Text>
-      <Button title="Login" onPress={loginAnonymously} />
-    </View>
-  );
+  handleSignInClick = () => {
+    // TODO
+  }
+
+  render() {
+    const { isSigninInProgress } = this.state;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.headingText}>Happy</Text>
+        <Text style={styles.headingText}>Fridge</Text>
+
+        <GoogleSigninButton
+          style={styles.signInButton}
+          size={GoogleSigninButton.Size.Wide}
+          color={GoogleSigninButton.Color.Dark}
+          onPress={this.handleSignInClick}
+          disabled={isSigninInProgress}
+        />
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  headingText: {
+    fontSize: 60,
+    fontWeight: 'bold',
+  },
+  signInButton: {
+    // Size is recommended from RN-google-signin
+    width: 312,
+    height: 48,
+    marginTop: 48,
   },
 });
 
