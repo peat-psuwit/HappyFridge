abd93fa34d172fe962039e5918b3dca0329c60c4
akairsslk <akairsslk@gmail.com>
2018-12-16 00:16:46 +0700

    CameraScreen using react-native-camera and can navigate to InputScreen with picture uri

--- src/screens/InputScreen.js
+++ src/screens/InputScreen.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image, StyleSheet } from 'react-native';
 
-function InputScreen() {
+function InputScreen({ navigation }) {
   // Stub
+  const imageData = navigation.getParam("imageData","there is no spoon");
   return (
-    <View>
+    <View style={styles.container} >
+      <Image source={{uri: imageData.uri}} style={styles.image} />
       <Text>InputScreen</Text>
+      <Text>{imageData.uri}</Text>
+      
     </View>
   );
 }
+// defaultSource={require('../images/not-found.png')}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'stretch',
+  },
+  image: {
+    flex: 0.4,
+    flexDirection: 'column',
+    alignItems: 'stretch',
+    backgroundColor: 'black',
+  }
+  });
 
 export default InputScreen;
