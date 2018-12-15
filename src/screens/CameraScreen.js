e34eeccee8777a85ecf3832484b0f87f9c52d502
akairsslk <akairsslk@gmail.com>
2018-12-16 04:52:31 +0700

    basic layout and function for InputScreen

--- src/screens/CameraScreen.js
+++ src/screens/CameraScreen.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Text, View, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
 import { RNCamera } from 'react-native-camera';
 
 function CameraScreen({ navigation }) {
--- src/screens/InputScreen.js
+++ src/screens/InputScreen.js
@@ -1,17 +1,59 @@
 import React from 'react';
-import { Text, View, Image, StyleSheet } from 'react-native';
+import { Text, View, Image, StyleSheet, TouchableOpacity, DatePickerAndroid } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
 
-function InputScreen({ navigation }) {
+class InputScreen extends React.Component {
   // Stub
-  const imageData = navigation.getParam("imageData","there is no spoon");
-  return (
-    <View style={styles.container} >
-      <Image source={{uri: imageData.uri}} style={styles.image} />
-      <Text>InputScreen</Text>
-      <Text>{imageData.uri}</Text>
-      
-    </View>
-  );
+  
+  state = {
+    foodType: '',
+    foodNum: '',
+    foodExpiredDate: new Date(),
+  };
+
+  openAndroidDatePicker = async function() {
+    try {
+      const {action, year, month, day} = await DatePickerAndroid.open({
+        date: new Date()
+      });
+      console.log(action, year, month, day);
+      if(action === DatePickerAndroid.dateSetAction){
+        let date = new Date(year, month, day);
+        this.setState({
+          foodExpiredDate: date
+        });
+      }
+      else if(action === DatePickerAndroid.dismissedAction){
+        console.log("User cancel DatePickerAndroid");
+      }
+    } catch ({code, message}) {
+      console.warn('Cannot open date picker', message);
+    }
+  }
+
+  render(){
+    const imageData = this.props.navigation.getParam("imageData","there is no spoon");
+    return (
+      <View style={styles.container} >
+        <Image source={{uri: imageData.uri}} style={styles.image} />
+        <Text>ประเภทอาหาร</Text>
+        <TextInput onChangeText={(text) => this.setState({foodType:text})} style={styles.textBox} />
+        <Text>ปริมาณ</Text>
+        <TextInput onChangeText={(text) => this.setState({foodNum:text})} keyboardType={"number-pad"} style={styles.textBox} />
+        <Text>หมดอายุ</Text>
+        <TextInput onChangeText={(text) => this.setState({foodType:text})} style={styles.textBox} />
+        <TouchableOpacity onPress={() => this.openAndroidDatePicker()} style={styles.textBox} >
+          <Text style={{ fontSize: 14 }}> {this.state.foodExpiredDate.toDateString()} </Text>
+        </TouchableOpacity>
+        <Text>{this.state.foodType}</Text>
+        <Text>InputScreen</Text>
+        <Text>{imageData.uri}</Text>
+      </View>
+   );
+  }
+
+
+
 }
 // defaultSource={require('../images/not-found.png')}
 
@@ -26,8 +68,11 @@ const styles = StyleSheet.create({
     flex: 0.4,
     flexDirection: 'column',
     alignItems: 'stretch',
-    backgroundColor: 'black',
+  },
+  textBox: {
+    borderBottomColor: 'gray',
+    borderBottomWidth: 1,
   }
-  });
+});
 
 export default InputScreen;
