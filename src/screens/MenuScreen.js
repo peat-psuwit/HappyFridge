9327c975f73ba0f8b9a1e1e404150f58dfb172e7
siryosdavit <sir_yosd_avit@hotmail.com>
2018-12-16 21:26:04 +0700

    commit for update master branch

--- src/screens/MenuScreen.js
+++ src/screens/MenuScreen.js
@@ -1,13 +1,43 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image, StyleSheet, } from 'react-native';
+import { firebase } from 'react-native-firebase';
 
-function MenuScreen() {
-  // Stub
-  return (
-    <View>
-      <Text>MenuScreen</Text>
-    </View>
-  );
+const 
+
+class MenuScreen  extends React.Component {
+  handleIngredientChange = () => {
+
+  }
+
+  render () {
+    
+
+
+    return (
+
+    )
+  }
 }
+
+// function MenuScreen() {
+//   // Stub
+
+//   return (
+//     <View>
+//       <Text>MenuScreen</Text>
+//     </View>
+//   );
+// }
+
+// const  styles = StyleSheet.create({
+//   container: {
+
+//   },
+  
+//   image: {
+    
+//   },
+  
+// });
 
 export default MenuScreen;
