343a5b02dfcdd0f20bbd1616b6436f715a03240f
siryosdavit <sir_yosd_avit@hotmail.com>
2018-12-17 18:36:27 +0700

    query menu

--- src/screens/MenuScreen.js
+++ src/screens/MenuScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, Image, StyleSheet, FlatList } from 'react-native';
+import { Text, View, Image, StyleSheet, FlatList, } from 'react-native';
 import firebase from 'react-native-firebase';
 
 import MenuEntry from './MenuEntry';
@@ -11,61 +11,72 @@ const firestore = firebase.firestore();
 class MenuScreen extends React.Component {
 
   state = {
-    user : {},
-    menu: {},
+    menu: {
+      ingredients: {},
+    },
   }
   
   getMenu = () => {
-    const menuId = this.props.navigation.getParam("menuId",'iTl3Pk4V0TMHabn7BYka');
+    const menuId = this.props.navigation.getParam("menuId");
     const auth = firebase.auth();
     const uid = auth.currentUser.uid;
     const menuRef = firestore.collection('menus');
+
     menuRef
-      .where('menus.id', '==', menuId)
+      .doc(menuId)
       .get().then(querySnapshot => {
-        querySnapshot.forEach(obj => {
-          console.log(obj.id);
+        console.log(querySnapshot)
+        this.setState({
+          menu: {id: querySnapshot.id, ...querySnapshot.data()}
         })
       })
 
-  }
+     
+    }
 
   componentDidMount(){
     this.getMenu();
-  }
+  };
 
-  handleIngredientChange = () => {
-    const 
-  }
+  handleIngredientChange = ({ingredients}) => {
+
+  };
 
   handleCookPressed = () => {
-
-  }
-
-  renderMenuInstruction = () => {
-
-  }
+     
+  };
 
 
-  render () {
-    const 
-
+  render () { 
+    // console.log(this.state.menu.ingredients); 
+    const test = Object.keys(this.state.menu.ingredients).map((key) => {
+      return {
+        id: key,
+        ...this.state.menu.ingredients[key]
+      }
+    });
+    // console.log(test);
     return(
       <View style={styles.container}>
         <Image 
           style = {styles.image}
             source={{uri: this.state.menu.picture}}
         />
-        <Text style = {style.header}> {this.state.menu.name} </Text>
+        <Text style = {styles.header}> {this.state.menu.name + "\n"} </Text>
         <Text style = {styles.bold}>ส่วนผสม</Text>
-        <FlatList 
-          data = {this.state.menu.ingredients}
-          renderItem = {({item}) => {
-            <Text> {"- " + item.name + item.defaultAmount + item.defaultUnit} </Text>
-          }}
-        />
+        <View>
+          <FlatList 
+            data = {test}
+            renderItem = {({item}) => {
+              console.log(item);
+              return (
+                <Text>{" - " + item.name + " " + item.defaultAmount + " " + item.defaultUnit }</Text>
+              );
+            }}
+          />
+        </View>
         <Text style = {styles.bold}>วิธีทำ</Text>
-        <Text> {this} </Text>
+        <Text> {this.state.menu.instruction} </Text>
         <TouchableItem
             style = {styles.fab}
             onPress={() => {
@@ -73,7 +84,7 @@ class MenuScreen extends React.Component {
             }}
           >
             <Image
-                  style={{width: 30, height: 30}}
+                  style={{width: 59, height: 59}}
                     source={{uri: 'https://cdn0.iconfinder.com/data/icons/food-drinks-glyph-black/2048/Fork_and_spoon-512.png'}}
             />
         </TouchableItem>
@@ -86,31 +97,34 @@ class MenuScreen extends React.Component {
 
 const  styles = StyleSheet.create({
   container: {
+    padding : 20,
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'flex-start',
     alignItems: 'stretch',
   },
   image: {
-    flex: 0.3,
+    flex: 0.4,
     flexDirection: 'column',
-    alignItems: 'stretch',
-    backgroundColor: 'black',
+    // alignItems: 'stretch',
+    // backgroundColor: 'black',
     
   },
   header: {
     fontWeight: 'bold',
-      fontSize: 25 ,
+      fontSize: 30 ,
   },
   bold: {
     fontWeight: 'bold',
-      fontSize: 20
+      fontSize: 17
   },
   fab: {
-    width: 31,
-    height: 31,
+    width: 60,
+    height: 60,
 
-
+    // flex: 0.1,
+    // // flexDirection: 'row-reverse',
+    // justifyContent: 'flex-end',
   },
   
 });
